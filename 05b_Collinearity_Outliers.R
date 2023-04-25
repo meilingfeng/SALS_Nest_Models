bf62ba38bde2290e4823a456141bbdef6b69f9d7
Mei-Ling Feng <mefeng7@gmail.com>
2023-04-24 21:07:51 -0400

    remove old candidate models and just compare all predictors

--- 05b_Collinearity_Outliers.R
+++ 05b_Collinearity_Outliers.R
@@ -33,12 +33,7 @@ ab_type
 
 
 #list all predictor variables in the full model
-full_mod<-mod_list_pres[[1]]
-full_mod_surv<-mod_list_surv[[1]]
-terms<-unlist(strsplit(as.character(full_mod$terms[[3]]),split=" * ", fixed=TRUE))%>%
-  strsplit(split=" + ", fixed=TRUE)%>%
-  unlist()
-vars<-terms[-c(1,4)]
+vars<-c(uvvr_mean,ndvi,HIMARSH,pca,ent_txt,cor_txt)
 
 
 pres_preds<-pres_train[,vars]
@@ -102,76 +97,4 @@ PerformanceAnalytics::chart.Correlation(pres_preds, histogram = TRUE, method = "
 # b) Look at variance inflation factor (larger if the covariate R2 is large, most variation in the covariate is explained by variation in all the other covariates)
 # remove variables with the highest VIF until all remaining variable have below a select threshold
 # even moderate collinearity can mask weak ecological signals so using a low threshold like 3 or 2 is important.
-
-# Full model
-# add to VIFs to model selection table (and AIC)
-vif_out<-vif(mod_list_pres[[1]])
-model_selection[1,"response"]<-"placement"
-model_selection[1,"mod_name"]<-"Full"
-model_selection[1,"mod_function"]<-deparse1(mod_list_pres[[1]]$formula)
-model_selection[1,"max_VIF"]<-max(vif_out)
-model_selection[1,"VIF_var"]<-names(vif_out[which(vif_out==max(vif_out))])
-model_selection[1,"AIC"]<-mod_list_pres[[1]]$aic
-
-vif_out<-vif(mod_list_surv[[1]])
-model_selection[2,"response"]<-"success"
-model_selection[2,"mod_name"]<-"Full"
-model_selection[2,"mod_function"]<-deparse1(mod_list_surv[[1]]$formula)
-model_selection[2,"max_VIF"]<-max(vif_out)
-model_selection[2,"VIF_var"]<-names(vif_out[which(vif_out==max(vif_out))])
-model_selection[2,"AIC"]<-mod_list_surv[[1]]$aic
-
-
-summary(mod_list_pres[[1]])
-
-#in response to VIF, success model has high VIF for pca, placement model high VIF for HIMARSH
-  # model with only binary high marsh
-mod_list_pres[[2]]<-update(mod_list_pres[[1]], .~.-HIMARSH)
-  # model without pca
-mod_list_surv[[2]]<-update(mod_list_surv[[1]], .~.-pca)
-    # add to VIFs to model selection table
-vif_out<-vif(mod_list_pres[[2]])
-model_selection[3,"response"]<-"placement"
-model_selection[3,"mod_name"]<-"High Marsh Presence"
-model_selection[3,"mod_function"]<-deparse1(mod_list_pres[[2]]$formula)
-model_selection[3,"max_VIF"]<-max(vif_out)
-model_selection[3,"VIF_var"]<-names(vif_out[which(vif_out==max(vif_out))])
-model_selection[3,"AIC"]<-mod_list_pres[[2]]$aic
-
-vif_out<-vif(mod_list_surv[[2]])
-model_selection[4,"response"]<-"success"
-model_selection[4,"mod_name"]<-"No PC"
-model_selection[4,"mod_function"]<-deparse1(mod_list_surv[[2]]$formula)
-model_selection[4,"max_VIF"]<-max(vif_out)
-model_selection[4,"VIF_var"]<-names(vif_out[which(vif_out==max(vif_out))])
-model_selection[4,"AIC"]<-mod_list_surv[[2]]$aic
-
-  # model with only high marsh proportions
-mod_list_pres[[3]]<-update(mod_list_pres[[1]], .~.-Highmarsh)
-mod_list_surv[[3]]<-update(mod_list_surv[[2]], .~.-Highmarsh)
-    # add to VIFs to model selection table
-vif_out<-vif(mod_list_pres[[3]])
-model_selection[5,"response"]<-"placement"
-model_selection[5,"mod_name"]<-"High Marsh Proportion"
-model_selection[5,"mod_function"]<-deparse1(mod_list_pres[[3]]$formula)
-model_selection[5,"max_VIF"]<-max(vif_out)
-model_selection[5,"VIF_var"]<-names(vif_out[which(vif_out==max(vif_out))])
-model_selection[5,"AIC"]<-mod_list_pres[[3]]$aic
-
-vif_out<-vif(mod_list_surv[[3]])
-model_selection[6,"response"]<-"success"
-model_selection[6,"mod_name"]<-"No PC, Only High Marsh Proportion"
-model_selection[6,"mod_function"]<-deparse1(mod_list_surv[[3]]$formula)
-model_selection[6,"max_VIF"]<-max(vif_out)
-model_selection[6,"VIF_var"]<-names(vif_out[which(vif_out==max(vif_out))])
-model_selection[6,"AIC"]<-mod_list_surv[[3]]$aic
-
-
-#All VIFs below 2 now. 
-
-
-
-#label the list of models
-names(mod_list_pres)<-c("Full","High Marsh Presence","High Marsh Proportion")
-names(mod_list_surv)<-c("Full","No PC","No PC, Only High Marsh Proportion")
 
