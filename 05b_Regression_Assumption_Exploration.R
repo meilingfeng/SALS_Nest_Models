9e7a45b0bb85cefb9e3dee7949c86bd66b1f3bf2
Mei-Ling Feng <mefeng7@gmail.com>
2023-06-15 16:58:32 -0400

    Explore nest data through GLM assumptions. Look for outliers, collinearity, interactions, linear relationships. Relationships non-linear, maybe use GAMs instead

---
+++ 05b_Regression_Assumption_Exploration.R
@@ -0,0 +1,175 @@
+library(tidyverse)
+library(car)#vif
+library(PerformanceAnalytics)#correlation plots
+library(patchwork)
+library(sf) #spatial data
+library(gstat) #variograms
+library(tseries) #autocorrelation
+
+### Set up
+# -------------------------------------------
+source("05a_Set_Model_Parameters.R")
+
+
+## 1. Look for outliers 
+#-------------------------------------------------------------------
+#list all predictor variables in the full model
+pres_preds<-pres_dat[,all_terms]
+surv_preds<-surv_dat[,all_terms]
+
+
+#Boxplot, histogram, scatterplot are graphical tools for outlier detection
+for(i in 1:(length(all_terms))){
+  par(mfrow = c(1, 3),mar=c(5,4,7,2)) #bottom, left, top, right
+  hist(pres_preds[,i], main = "Histogram")
+  boxplot(pres_preds[,i], main = "Boxplot")
+  qqnorm(pres_preds[,i], main = "Normal Q-Q plot")
+  mtext(paste0(all_terms[i]), side = 3, line = - 2, outer = TRUE)
+}
+
+
+## 2. Is there collinearity among the covariates?
+#-------------------------------------------------------------
+# a) plot pairwise scatterplots, (alternatively look at correlation coefficients, or a PCA biplot of all covariates)
+PerformanceAnalytics::chart.Correlation(pres_preds, histogram = TRUE, method = "spearman")
+#entropy, pca, and ndvi correlated
+#try removing entropy all_terms[-5]
+
+# b) Look at variance inflation factor (larger if the covariate R2 is large, most variation in the covariate is explained by variation in all the other covariates)
+# remove variables with the highest VIF until all remaining variable have below a select threshold
+# even moderate collinearity can mask weak ecological signals so using a low threshold like 3 or 2 is important.
+mod.p<-glm(as.formula(paste0("y~",paste(all_terms,collapse = "+"))), pres_dat,family=binomial(link="logit"))
+vif(mod.p)
+mod.s<-glm(as.formula(paste0("y~",paste(all_terms,collapse = "+"))), surv_dat,family=binomial(link="logit"))
+vif(mod.s)
+#keeping entropy seems fine, no VIFs above 3
+
+
+
+## 3. Should we consider interactions?
+#---------------------------------------------------
+# Hypothesis: The quality (resiliency) of high marsh is more attractive for nest building and leads to more successful nests. 
+# Interactions: Areas with more high marsh AND 
+              # lower mean UVVR (more vegetated/resilient)
+              # 0-negative UVVR change (stable resilience or improved vegetation cover)
+              # certain value of reflectance that indicates an unmeasured habitat characteristic
+              # higher NDVI (healthier veg)
+# within the high marsh, do locations with (successful) nests have higher NDVI, etc?
+
+#high marsh used for nesting seems to have distinct characteristics from the rest of the high marsh area
+# adding more high marsh habitat also adds higher PCA values for nesting sites, sparrows are picking high marsh habitat with certain reflective qualities.
+p1<-ggplot(pres_dat,aes(x=HIMARSH,y=pca,group=as.factor(y)))+
+  geom_point(aes(color=as.factor(y)))+
+  geom_smooth(method="lm",aes(linetype=as.factor(y)),color="black")+
+  scale_color_manual(values=c("#5ab4ac","#d8b365"))+
+  labs(color = "Nest Site", linetype= "Nest Site",y = "PC Spectral (RBG)", x="Proportion High Marsh") + 
+  theme_classic(base_size = 12)
+# High marsh used for nesting also has a higher level of NDVI (vegetation vigor) than the rest of the high marsh area
+p2<-ggplot(pres_dat,aes(x=HIMARSH,y=ndvi,group=as.factor(y)))+
+  geom_point(aes(color=as.factor(y)))+
+  geom_smooth(method="lm",aes(linetype=as.factor(y)),color="black")+
+  scale_color_manual(values=c("#5ab4ac","#d8b365"))+
+  labs(color = "Nest Site", linetype= "Nest Site",y = "NDVI", x="Proportion High Marsh") + 
+  theme_classic(base_size = 12)
+
+
+p1/p2
+ggsave(filename=paste0(path_out,"Intermediate_outputs/Data_Model_Exploration/himarsh_quality_interactions_placement_bp.png"), width = 7, height = 10, dpi = "retina")
+
+# successful nests are in high marsh with higher levels of NDVI
+ggplot(surv_dat,aes(x=HIMARSH,y=ndvi,group=as.factor(y)))+
+  geom_point(aes(color=as.factor(y)))+
+  geom_smooth(method="lm",aes(linetype=as.factor(y)),color="black")+
+  scale_color_manual(values=c("#5ab4ac","#d8b365"))+
+  labs(color = "Nest Success", linetype= "Nest Success",y = "NDVI", x="Proportion High Marsh") + 
+  theme_classic(base_size=12)
+ggsave(filename=paste0(path_out,"Intermediate_outputs/Data_Model_Exploration/himarsh_quality_interactions_success.png"), width = 7, height = 6, dpi = "retina")
+
+
+# Hypothesis: Important habitat characteristics change across the breeding range
+# high marsh appears to be less important for successful nesting at higher latitudes
+p3<-ggplot(surv_dat,aes(x=latitude,y=HIMARSH,group=as.factor(y)))+
+  geom_point(aes(color=as.factor(y)))+
+  geom_smooth(method="lm",aes(linetype=as.factor(y)),color="black")+
+  scale_color_manual(values=c("#5ab4ac","#d8b365"))+
+  labs(color = "Nest Success", linetype= "Nest Success",y = "Proportion High Marsh", x="Latitude") + 
+  theme_classic(base_size=12)
+
+p4<-ggplot(pres_dat,aes(x=latitude,y=HIMARSH,group=as.factor(y)))+
+  geom_point(aes(color=as.factor(y)))+
+  geom_smooth(method="lm",aes(linetype=as.factor(y)),color="black")+
+  scale_color_manual(values=c("#5ab4ac","#d8b365"))+
+  labs(color = "Nest Site", linetype= "Nest Site",y = "Proportion High Marsh", x="") + 
+  theme_classic(base_size=12)
+p4/p3
+ggsave(filename=paste0(path_out,"Intermediate_outputs/Data_Model_Exploration/himarsh_lat_interactions_bp.png"), width = 7, height = 10, dpi = "retina")
+
+
+# conclusions: consider HIMARSH*pca (just presence), HIMARSH*NDVI, HIMARSH*latitude
+
+
+## 4. Are response observations independent?
+#----------------------------------------------------------------------
+# inflates type 1 error or false positives and raises p values in regression
+
+#Good to check dependence before and after modeling
+# plot response against spatial and temporal variables
+# check that model residuals do not have dependence structure
+
+#formal way to check is with autocorrelation
+#plot auto correlation funcitons (ACF) for regular time series (pearsons correlation between time series and lagged time series)
+#plot variograms for irregularly spaced time series and spatial data
+# straight diagonal line indicates perfect correlation. Want it to reach a sill where the linear trend devolves
+
+# variograms https://csegrecorder.com/articles/view/the-variogram-basics-a-visual-intro-to-useful-geostatistical-concepts 
+
+#get residuals
+pres_dat$res<-mod.p$residuals  
+surv_dat$res<-mod.s$residuals 
+
+# spatial autocorrelation - doesn't seem to be any
+dat2<-st_as_sf(surv_dat, coords=c("longitude","latitude"))
+dat3<-st_as_sf(surv_dat, coords=c("longitude","latitude"))
+
+gram1<-variogram(res~1,data=dat2)  #filter(dat2,site=="ER")
+plot(gram1)
+gram2<-variogram(res~1,data=dat3)  #filter(dat2,site=="ER")
+plot(gram2)
+
+
+# temporal autocorrelation
+dat2.2<-dat2%>%group_by(site,Year)%>%summarise(res=mean(res))%>%
+  arrange(site,Year)%>%
+  mutate(lag1=lag(res,n=1),
+         lag2=lag(res,n=2))
+
+plot(dat2.2$res,dat2.2$lag2,xlab="t",ylab="t-1")
+
+
+#plot residuals over time and lat
+plot(pres_dat$res~pres_dat$latitude)
+
+plot(surv_dat$res~surv_dat$latitude)
+
+plot(pres_dat$res~pres_dat$Year)
+
+plot(surv_dat$res~surv_dat$Year)
+
+stripchart(surv_dat$res~surv_dat$site,pch=1,method="jitter")
+
+
+## 5. Are relationships between response and predictors linear?
+pres_long<-pivot_longer(pres_dat,c(all_of(all_terms)),names_to = "var",values_to = "value")
+ggplot(pres_long, aes(x = value, y = y)) +
+  geom_point()+
+  geom_smooth(method="gam", method.args = list( family = "binomial"))+
+  facet_wrap(~var,scales = "free")
+## suggest a strong non-linearlity, although an odd one                    
+ggplot(surv_dat, aes(x = log(ndvi+0.01), y = y)) +
+  geom_smooth(method="gam", method.args = list( family = "binomial")) +
+  theme_bw()
+ggplot(surv_dat, aes(x = ndvi, y = y)) +
+  geom_smooth(method="gam", method.args = list( family = "binomial")) +
+  theme_bw()
+#log ndvi
+
