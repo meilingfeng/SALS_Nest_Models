a663ebc02c083e2b0a87b7d0273740d9b615375d
Mei-Ling Feng <mefeng7@gmail.com>
2023-06-26 10:55:40 -0400

    Reorganize code formatting and aligning predictor rasters. Do all raster editing first, then buffer the coarser resolution rasters that do not align with the marsh polygon rasters from correll et al. Align rasters last to create prediction surface.

---
+++ 02a_Buffer_and_Format_Rasters.R
@@ -0,0 +1,258 @@
+
+library(tidyverse)
+library(terra)
+library(GLCMTextures)
+
+#######################################################################################
+## extend the edges of each raster so that all predictor variables cover all (most) nest locations
+#######################################################################################
+
+
+
+## Set up file paths
+# -------------------------------------------
+dat_path<-"D:/Nest_Models/Data/" #input data
+path_out<-"D:/Nest_Models/Outputs/" #outputs
+
+
+
+
+
+## Load coarse resolution raster data
+#----------------------------------------------------------------------------
+
+## Use focal zone of 9x9 cells for coarse data (30m)
+coarse_buff<-function(x){terra::focal(rast(x), w=9, fun=mean,na.policy="only", na.rm=T)}
+
+## 1. UVVR
+######
+  # transformed uvvr coordinate system from WGS to NAD83 in ArcPro
+  # just want band 4 (UVVR). Band 1 is unveg, 2 is veg, and 3 is proportion of water.
+
+  # first remove outliers from UVVR data. Values should not exceed 2 or be under 0.
+if(!file.exists(paste0(path_out,"Intermediate_outputs/UVVR/uvvr_mean_noOutlier.tif"))){ # check if the file already exists to prevent overwriting it
+  
+  # A. UVVR mean across 2014-2018
+  uvvr<-rast(paste0(dat_path,"UVVR/UVVR_annual_mean/uvvr_mean_utm18_2.tif"))%>%
+    dplyr::rename_with(function(x){x<-'uvvr_mean'},.cols = everything()) 
+  # values above 2 are not accurate, set to NA
+  uvvr[uvvr>2]<-NA
+  
+  # B. UVVR change (2014-2018)
+  uvvr14<-rast(paste0(dat_path,"UVVR/uvvr_14_utm18_mean_ext.tif"))%>%
+    dplyr::rename_with(function(x){x<-'uvvr_14'},.cols = everything())
+  # values above 2 are not accurate, set to NA
+  uvvr14[uvvr14>2]<-NA
+  uvvr18<-rast(paste0(dat_path,"UVVR/uvvr_18_utm18_mean_ext.tif"))%>%
+    dplyr::rename_with(function(x){x<-'uvvr_18'},.cols = everything())
+  uvvr18[uvvr18>2]<-NA
+  # subtract 2018 from 2014 to get change
+  uvvr_diff<-uvvr18-uvvr14
+  uvvr_diff<-uvvr_diff%>%
+    dplyr::rename_with(function(x){x<-'uvvr_diff'},.cols = everything())
+  
+  
+  writeRaster(uvvr,filename = paste0(path_out,"Intermediate_outputs/UVVR/uvvr_mean_noOutlier.tif"),overwrite=T)
+  writeRaster(uvvr_diff,filename = paste0(path_out,"Intermediate_outputs/UVVR/uvvr_diff_noOutlier.tif"),overwrite=T)
+  
+}
+
+  # now buffer the NDVI rasters with outliers removed
+
+if(!file.exists(paste0(path_out,"Intermediate_outputs/UVVR/uvvr_diff_noOutlier_buff.tif"))){
+  # create buffer
+uvvr_dif<-coarse_buff(paste0(path_out,"Intermediate_outputs/UVVR/uvvr_diff_noOutlier.tif"))
+  # write file
+writeRaster(uvvr_dif,paste0(path_out,"Intermediate_outputs/UVVR/uvvr_diff_noOutlier_buff.tif"),overwrite=T)
+
+  # create buffer
+uvvr_mean<-coarse_buff(paste0(path_out,"Intermediate_outputs/UVVR/uvvr_mean_noOutlier.tif"))
+  # write file
+writeRaster(uvvr_mean,paste0(path_out,"Intermediate_outputs/UVVR/uvvr_mean_noOutlier_buff.tif"),overwrite=T)
+
+}
+
+
+
+
+
+## 2. Precipitation
+#####
+if(!file.exists(paste0(path_out,"Intermediate_outputs/Precip/precip_buff.tif"))){
+  
+  # create buffer
+  precip<-coarse_buff(paste0(dat_path,"Precip/PRISM_ppt_30yr_normal_800mM4_annual_bil.bil"))
+  # write file
+  writeRaster(precip,paste0(path_out,"Intermediate_outputs/Precip/precip_buff.tif"),overwrite=T)
+}
+
+
+
+
+
+## 3. Tidal restrictions
+#####
+if(!file.exists(paste0(path_out,"Intermediate_outputs/Tidal_restriction/tideres_buff.tif"))){
+  
+  # create buffer
+  tideres<-coarse_buff(paste0(dat_path,"DSL_tidal_restrictions/tideres_2020_v5.0.tif"))
+  # write file
+  writeRaster(tideres,paste0(path_out,"Intermediate_outputs/Tidal_restriction/tideres_buff.tif"),overwrite=T)
+}
+
+
+
+
+
+
+## Load fine resolution raster data
+#---------------------------------------------------------------------
+## Use focal zone of 17x17 cells for fine data (3m)
+fine_buff<-function(x){terra::focal(rast(x), w=17, fun=mean,na.policy="only", na.rm=T)}
+
+
+## 1. Texture
+#####
+
+if(!file.exists(paste0(path_out,"Intermediate_outputs/Texture/ent_txt_1.tif"))){
+
+  # first calculate neighborhood texture metrics
+  
+    # Raster Quanitization 
+      # use equal range method when comparing across several datasets (can set gloabl min and max range using max_val and min_val) - splits data into equal ranges
+      # alternative option is "equal prob" which splits by quantiles, used in original paper (Haralick and Shanmugam 1973)
+      # NDVI of live plants ranges 0:1, set range and breaks at 0.1 intervals
+  ndvi<-map(unlist(map(paste0(dat_path,"Correll_NAIP"),~list.files(.,pattern = "NDVI.tif$",full.names=T))),rast)
+  txt_entro<-txt_corr<-list()
+  
+  for (i in 1:length(ndvi)){
+    ndvi_rq<- quantize_raster(r = ndvi[[i]], n_levels = 10, method = "equal range")
+    
+    # Calculate texture metrics
+      # use window size of 3x3m 
+      # calculate across multiple shifts (invariant)
+    txt<- glcm_textures(ndvi_rq, w = c(3,3), n_levels =10, 
+                        metrics = c("glcm_entropy", "glcm_correlation"),
+                        quantization = "none", shift = list(c(1, 0), c(1, 1), c(0, 1), c(-1, 1)))
+    
+    txt_entro[[i]]<-txt[[1]]
+    txt_corr[[i]]<-txt[[2]]
+    
+  }
+  
+  # write newly generated layers to file
+  
+  for(i in 1:length(txt_entro)){
+    writeRaster(txt_entro[[i]],filename=paste0(path_out,"Intermediate_outputs/Texture/ent_txt",i,".tif"),overwrite=T)
+    writeRaster(txt_corr[[i]],file=paste0(path_out,"Intermediate_outputs/Texture/cor_txt",i,".tif"),overwrite=T)
+  }
+}
+
+
+  # then buffer the rasters
+if(!file.exists(paste0(path_out,"Intermediate_outputs/Texture/ent_txt_1_buff.tif"))){
+  
+  # list files
+cor_list<-unlist(map(paste0(path_out,"Intermediate_outputs/Texture"),~list.files(.,pattern = "^cor_txt_[0-9].tif$",full.names=T)))
+  # apply buffer
+cor_list<-map(cor_list,fine_buff)
+  #write buffer files
+for(i in 1:length(cor_list)){
+  writeRaster(cor_list[[i]],paste0(path_out,"Intermediate_outputs/Texture/cor_txt_",i,"_buff.tif"))
+}
+
+  # list files
+ent_list<-unlist(map(paste0(path_out,"Intermediate_outputs/Texture"),~list.files(.,pattern = "^ent_txt_[0-9].tif$",full.names=T)))
+  # apply buffer
+ent_list<-map(ent_list,fine_buff)
+  # write buffer files
+for(i in 1:length(ent_list)){
+  writeRaster(ent_list[[i]],paste0(path_out,"Intermediate_outputs/Texture/ent_txt_",i,"_buff.tif"))
+}
+
+}
+
+
+
+## 2. NDVI
+#####
+
+  # first buffer the original layer
+if(!file.exists(paste0(path_out,"Intermediate_outputs/NDVI/1_NDVI_buff.tif"))){
+  # list files
+  ndvi_list<-unlist(map(paste0(dat_path,"Correll_NAIP"),~list.files(.,pattern = "NDVI.tif$",full.names=T)))
+  # apply buffer
+  ndvi_list<-map(ndvi_list,fine_buff)
+  #write buffer files
+  for(i in 1:length(ndvi_list)){
+      writeRaster(ndvi_list[[i]],paste0(path_out,"Intermediate_outputs/NDVI/",i,"_NDVI_buff.tif"),overwrite=T)
+    }
+}
+
+  # then also set all negative values in NDVI to zero. All negative values are non-plant matter.
+if(!file.exists(paste0(path_out,"Intermediate_outputs/NDVI/Z1_zeroed_NDVI_buff.tif"))){
+  ndvi_list<-map(unlist(map(paste0(path_out,"Intermediate_outputs"),~list.files(.,pattern = "NDVI_buff.tif$",full.names=T))),rast)
+  #set values below 0 NDVI to 0 - barren land or water
+  for(i in 1:length(ndvi_list)){
+    dat<-ndvi_list[[i]]
+    dat[dat<0]<-0
+    ndvi_list[[i]]<-dat
+  }
+  for(i in 1:length(ndvi_list)){
+    writeRaster(ndvi_list[[i]],paste0(path_out,"Intermediate_outputs/NDVI/Z",i,"_zeroed_NDVI_buff.tif"),overwrite=T)
+  }
+}
+
+
+## 3. PCA *** continue here
+#####
+
+  # PCA raster has multiple layers, one for each principal component. We just want the first layer, PC1.
+  # First extract PC1 to its own raster layer
+if(!file.exists(paste0(path_out,"Intermediate_outputs/PCA/Z1_PC1.tif"))){
+  # list files
+  pca_list<-unlist(map(paste0(dat_path,"Correll_NAIP"),~list.files(.,pattern = "PCA.tif$",full.names=T)))
+  #extract just the first band for the pca data
+  mapply(function(x,y){writeRaster(rast(x,lyrs=1),paste0(path_out,"Intermediate_outputs/PCA/Z",y,"_PC1.tif"),overwrite=T)},
+         pca_list,c(1:length(pca_list)))
+}
+
+  # Now run buffer
+if(!file.exists(paste0(path_out,"Intermediate_outputs/PCA/Z1_PC1_buff.tif"))){
+  # list files
+  pca_list<-unlist(map(paste0(path_out,"Intermediate_outputs/PCA"),~list.files(.,pattern = "PC1.tif$",full.names=T)))
+  # apply buffer
+  pca_list<-map(pca_list,fine_buff)
+  #write buffer files
+  for(i in 1:length(pca_list)){
+    writeRaster(pca_list[[i]],paste0(path_out,"Intermediate_outputs/PCA/Z",i,"_PC1_buff.tif"),overwrite=T)
+  }
+}
+
+
+
+
+
+## 4. High marsh
+#####
+if(!file.exists(paste0(path_out,"Intermediate_outputs/HIMARSH/Z1_vegcls_buff.tif"))){
+#list raster files
+file_list<-unlist(map(paste0(dat_path,"Correll_Marsh_Zones"),~list.files(.,pattern = "DEM.tif$",full.names=T)))
+
+#read as raster layers
+vg_cls<-map(file_list,rast)
+
+mode_buff<-function(x){
+  out<-terra::focal(x, w=9, fun=modal, na.policy="only",na.rm=T)
+}
+
+vg_cls<-map(vg_cls,mode_buff)
+
+for (i in 1:length(vg_cls)){
+  writeRaster(vg_cls[[i]],paste0(path_out,"Intermediate_outputs/HIMARSH/Z",i,"_vegcls_buff.tif"))
+}
+
+}
+t<-mode_buff(vg_cls[[5]])
+
+
--- 02b_Align_Rasters_to_Predictor_Surface.R
+++ 02b_Align_Rasters_to_Predictor_Surface.R
@@ -1,39 +1,45 @@
 library(tidyverse)
+#spatial analysis
 library(sf)
+#raster analysis
 library(terra)
 library(tidyterra)
-library(exactextractr)
-#gdal, for gdalwarp (manupilating raster projections)
+#library(exactextractr)# 
+#gdal, for gdalwarp (manupilating/aligning raster projections)
 #first download gdal here https://gdal.org/download.html#current-release
 #unzip gdal using this technique onto your C drive https://www.makeuseof.com/what-is-gz-file/
 #library(devtools)
 #install_github("JoshOBrien/gdalUtilities")
 library(gdalUtilities)#raster manipulation
 
-
+##################################################################################################################
 ## Create prediction surface
 # ---------------------------------------------------------------------------------------------
 # align extent and resolution of all environmental rasters
-# use a prediction surface resolution of 3 meters (matches the Correll data, plus middle ground between NAIP and UVVR)
+# use a prediction surface resolution of 30 meters based on min distance between nests and available computing power
 # Set all coordinate systems to ESPG:4269 (NAD 1983 geographic coord system)
-# Use Atlantic coast marsh extent (refer to Correll marsh layer) for each zone (n = 8)- predict by zone to reduce computation time
+# Use Atlantic coast marsh extent (Correll marsh layer) for each zone (n = 8)- predict by zone to reduce computation time
+######################################################################################################################
 
+
+## Set up
+#--------------------------------------------------------------------------
 #file path names
 dat_path<-"D:/Nest_Models/Data/"
 path_out<-"D:/Nest_Models/Outputs/"
 
-#functions
-source("C:/Users/mefen/OneDrive/Documents/Github/UCONN/SHARP/Functions/randomPoints.R")
+#functions (to select random background points)
+source("C:/Users/mefen/OneDrive/Documents/Github/SHARP/Functions/randomPoints.R")
 
-#nest data
+# cleaned and filtered focal species nest data
 nests<-st_read(paste0(path_out,"Final_outputs/Nest_locations/SALS_nests_2010_2020_dist_err_removed.shp"))
 
 
 
-## 1. Create prediction surface templates for each zone
+## 1. Create prediction surface templates for each zone (this is what we will align the other raster layers to)
 #---------------------------------------------------------------------------
 
-# Get zone extents from Correll Marsh Veg data
+# Get the zone extents from Correll Marsh Veg data
 
 #list raster files
 file_list<-unlist(map(paste0(dat_path,"Correll_Marsh_Zones"),~list.files(.,pattern = "DEM.tif$",full.names=T)))
@@ -50,50 +56,33 @@ temps<-lapply(vg_cls,function(x)rast(extent=ext(x),crs="EPSG:26918",resolution=r
 
 
 
-## 2. load all the remaining predictors
+## 2. load all the environmental predictors
 #---------------------------------------------------------------------------
-#UVVR
-uvvr_dif<-paste0(path_out,"Final_outputs/UVVR/uvvr_diff_noOutlier.tif")
-uvvr_mean<-paste0(path_out,"Final_outputs/UVVR/uvvr_mean_noOutlier.tif")
-
-#NAIP
-#list raster files
-cor_list<-unlist(map(paste0(path_out,"Final_outputs/NAIP_texture"),~list.files(.,pattern = "^cor_txt_[0-9].tif$",full.names=T)))
-ent_list<-unlist(map(paste0(path_out,"Final_outputs/NAIP_texture"),~list.files(.,pattern = "^ent_txt_[0-9].tif$",full.names=T)))
-hom_list<-unlist(map(paste0(path_out,"Final_outputs/NAIP_texture"),~list.files(.,pattern = "^hom_txt_[0-9].tif$",full.names=T)))
-
-if(!file.exists(paste0(path_out,"Final_outputs/Correll_NAIP/Z1_zeroed_NDVI.tif"))){
-ndvi_list<-map(unlist(map(paste0(dat_path,"Correll_NAIP"),~list.files(.,pattern = "NDVI.tif$",full.names=T))),rast)
-#set values below 0 NDVI to 0 - barren land or water
-for(i in 1:length(ndvi_list)){
-  dat<-ndvi_list[[i]]
-  dat[dat<0]<-0
-  ndvi_list[[i]]<-dat
-}
-for(i in 1:length(ndvi_list)){
-  writeRaster(ndvi_list[[i]],paste0(path_out,"Final_outputs/Correll_NAIP/Z",i,"_zeroed_NDVI.tif"))
-}
-}
-
-ndvi_list<-unlist(map(paste0(path_out,"Final_outputs/Correll_NAIP"),~list.files(.,pattern = "_zeroed_NDVI.tif$",full.names=T)))
-
-if(!file.exists(paste0(path_out,"Final_outputs/Correll_NAIP/Z1_PC1.tif"))){
-pca_list<-unlist(map(paste0(dat_path,"Correll_NAIP"),~list.files(.,pattern = "PCA.tif$",full.names=T)))
-  #extract just the first band for the pca data
-mapply(function(x,y){writeRaster(rast(x,lyrs=1),paste0(path_out,"Final_outputs/Correll_NAIP/Z",y,"_PC1.tif"))},
-       pca_list,c(1:length(pca_list)))
-}
-
-pca_list<-unlist(map(paste0(path_out,"Final_outputs/Correll_NAIP"),~list.files(.,pattern = "PC1.tif$",full.names=T)))
-
+#UVVR 
+uvvr_dif<-paste0(path_out,"Intermediate_outputs/UVVR/uvvr_diff_noOutlier_buff.tif")
+uvvr_mean<-paste0(path_out,"Intermediate_outputs/UVVR/uvvr_mean_noOutlier_buff.tif")
 
 #Precipitation
-precip<-paste0(dat_path,"Precip/PRISM_ppt_30yr_normal_800mM4_annual_bil.bil")
+precip<-paste0(path_out,"Intermediate_outputs/Precip/precip_buff.tif")
 
 # Tidal restrictions
-tideres<-paste0(dat_path,"DSL_tidal_restrictions/tideres_2020_v5.0.tif")
+tideres<-paste0(path_out,"Intermediate_outputs/Tidal_restriction/tideres_buff.tif")
 
-#combine data that are at the full extent
+# Texture
+#list raster files
+cor_list<-unlist(map(paste0(path_out,"Intermediate_outputs/Texture"),~list.files(.,pattern = "^cor_txt_[0-9]_buff.tif$",full.names=T)))
+ent_list<-unlist(map(paste0(path_out,"Intermediate_outputs/Texture"),~list.files(.,pattern = "^ent_txt_[0-9]_buff.tif$",full.names=T)))
+
+# NDVI
+ndvi_list<-unlist(map(paste0(path_out,"Intermediate_outputs/NDVI"),~list.files(.,pattern = "_zeroed_NDVI_buff.tif$",full.names=T)))
+
+# Principal Component of raw NAIP reflection bands (R, G, B)
+pca_list<-unlist(map(paste0(path_out,"Intermediate_outputs/PCA"),~list.files(.,pattern = "PC1.tif$",full.names=T)))
+
+
+
+
+#combine data that are at the full range extent
 file_list1<-c(uvvr_dif,uvvr_mean,precip,tideres)
 
 #combine data that are at zone extents
@@ -102,15 +91,15 @@ file_list2<-unlist(lapply(files2,sort))
 
 
 
-## 3. Align full extent predictors to templates
+## 3. Align full extent predictors to zone templates
 #---------------------------------------------------------------------------
 
-#for each full extent dataset
-for(j in 2:length(file_list1)){
+#for each full extent dataset...
+for(j in 1:length(file_list1)){
   
-  input<-file_list1[[j]]
+  input<-file_list1[[j]] # take 1 individual dataset
 
-  # First set the resampling method and output data type to match the input data
+  # First set the resampling method and make the output data type match the input data type
   #####
     #if the data is numeric (decimals), use average resampling
     #if the data is nominal (classes), use mode resampling
@@ -127,9 +116,10 @@ for(j in 2:length(file_list1)){
   
   # Then align the input data to each of the zone templates
   #####
+    # for each zone template...
   for(i in 1:length(temps)){
     
-    #create output file name
+    #create an output file name by referencing the input dataset
     output<-paste(substring(file_list1[[j]], 
                             #take the spot just before .tif in the file name
                             c(1,nchar(file_list1[[j]])-3), 
@@ -158,7 +148,7 @@ for(j in 2:length(file_list1)){
 
 
 
-## 4. Align zone extent predictors to templates
+## 4. Align zone extent predictors to zone templates
 #---------------------------------------------------------------------------
 
 #define alignment function
@@ -209,9 +199,12 @@ align_z_rast<-function(in_file,temp_list,collapse_string){
 }
 
 
+
+
 #create a repeating list of templates for each dataset
 temp_order<-rep(temps,times = length(files2))
-#apply the raster alignment to each dataset zone and template zone pair
+
+#apply the raster alignment function to each dataset and template zone pair
 mapply(align_z_rast,file_list2,temp_order,collapse_string=paste0("_",reso,"align"))
 
 
@@ -230,63 +223,55 @@ mapply(align_z_rast,file_list2,temp_order,collapse_string=paste0("_",reso,"align
 #STREAM=7
 #POOL/PANNE=6
 
-#(focus on HIMARSH, LOMARSH for now)
+#(focus on HIMARSH for now)
+
 hi_files<-list()
-lo_files<-list()
 
+if (!file.exists(paste0(path_out,"Intermediate_outputs/HIMARSH/Z1_himarsh_",reso,".tif"))) {
+  
 for(i in 1:length(vg_cls)){
-#write the new raster with veg class proportions to file
-hi_files[[i]]<-paste0(path_out,"Final_outputs/Correll_Marsh_Zones/Z",i,"_himarsh_",reso,".tif")
-lo_files[[i]]<-paste0(path_out,"Final_outputs/Correll_Marsh_Zones/Z",i,"_lomarsh_",reso,".tif")
-
-#if the file does not exist...
-if (!file.exists(hi_files[[i]])) {
-#break the veg layer into its classes by setting each veg class value to 1 separately
-hi<-lo<-vg_cls[[i]]
+# distinguish high marsh by setting high marsh values to 1 and all other values to 0
+hi<-vg_cls[[i]]
 hi[hi!=1]<-0
-lo[lo!=2]<-0
 
-#count the number of 3x3m cells within each larger raster cell 30x30, using aggregate
+#count the number of 3x3m cells within each larger raster cell (30x30), using aggregate
 hi<-terra::aggregate(hi,fact=10,fun='sum',dissolve=F)
-lo<-terra::aggregate(hi,fact=10,fun='sum',dissolve=F)
 
 #divide count by 100 to get proportion
 hi<-hi/100
-lo<-lo/100
 
 #give the variable a name
 hi<-dplyr::rename_with(hi,function(x){x<-'HIMARSH'},.cols = everything())
-lo<-dplyr::rename_with(hi,function(x){x<-'LOMARSH'},.cols = everything())
+
+#create output file names for the high marsh proportions 
+hi_files[[i]]<-paste0(path_out,"Intermediate_outputs/HIMARSH/Z",i,"_himarsh_",reso,".tif")
 
 #write the new raster with veg class proportions to file
 writeRaster(hi,filename=hi_files[[i]], overwrite=TRUE)
-writeRaster(lo,filename=lo_files[[i]], overwrite=TRUE)
 }
 
+}
+
+for(i in 1:length(vg_cls)){
+hi_files[[i]]<-paste0(path_out,"Intermediate_outputs/HIMARSH/Z",i,"_himarsh_",reso,".tif")
 }
 
 #then align these to the templates
 #apply the raster alignment to each dataset zone and template zone pair
 mapply(align_z_rast,hi_files,temps,"align")
-mapply(align_z_rast,lo_files,temps,"align")
-  
 
 
 
 
-
-
-
-## 5. combine all prediction varaibles into a single dataframe by cell ID
+## 5. combine all prediction variables into a single dataframe by cell ID
 #---------------------------------------------------------------------------
 #Bring in all the aligned data
 file_list1_align<-list()
 file_list2_align<-list()
 hi_align<-c()
-lo_align<-c()
 set_t<-c()
 
-#full extent datasets
+# get a list of file names for full extent datasets
   for (j in 1:length(file_list1)){
   for(i in 1:length(temps)){
   
@@ -304,7 +289,7 @@ set_t<-c()
 file_list1_align<-unlist(file_list1_align)
 
 
-#zone extent datasets
+# get a list of file names for zone extent datasets
 file_list2_align<-unlist(map(file_list2,function(input){
   paste(substring(input, 
                 #take the spot just before .tif in the file name
@@ -316,7 +301,7 @@ file_list2_align<-unlist(map(file_list2,function(input){
 ))
 
 
-# veg proportion data
+# get high marsh proportion file names
 hi_align<-unlist(map(hi_files,function(input){
   paste(substring(input, 
                        #take the spot just before .tif in the file name
@@ -327,21 +312,13 @@ hi_align<-unlist(map(hi_files,function(input){
 }
 ))
 
-lo_align<-unlist(map(lo_files,function(input){
-  paste(substring(input, 
-                       #take the spot just before .tif in the file name
-                       c(1,nchar(input)-3), 
-                       c(nchar(input)-4,nchar(input))), 
-             # and add in the aligned resolution and zone to the output name
-             collapse="align")
-}
-))
 
-all_files<-c(file_list2_align,file_list1_align,hi_align,lo_align)
+# combine the file names of all aligned predictor variables into a list
+all_files<-c(file_list2_align,file_list1_align,hi_align)
 
 
 
-#give each template cell a unique id
+#give each predictor surface template cell a unique id
 ids<-map(all_files[c(1:8)],rast)
 for(i in 1:length(ids)){
   id<-ids[[i]]
@@ -364,13 +341,13 @@ for(i in seq(1,length(all_files),by=8)){
   file_list_zone[i]<-all_files[(i+j-1)]
   file_list_zone<-file_list_zone[file_list_zone!="NULL"]
 }
-  dat_zone<-rast(unlist(file_list_zone))
-  dat_zone<-rast(list(dat_zone,ids[[j]]))
-  dat_zone<-mask(dat_zone,ids[[j]])
-  names(dat_zone)<-c("vg_clss","cor_txt","ent_txt","NDVI","PC1","uvvr_diff","uvvr_mean","precip","tideres","himarsh","lomarsh","id")
-  mat[[j]]<-as.data.frame(terra::as.matrix(dat_zone[-c(3,6,8,10)],wide=F))%>%#remove predictors not being used (cor, uvvr_diff, precip, lomarsh)
-    mutate(id=paste0(id,"z",j))%>%
-    filter(vg_clss!="NaN")
+  #dat_zone<-rast(unlist(file_list_zone))
+  #dat_zone<-rast(list(dat_zone,ids[[j]]))
+  #dat_zone<-mask(dat_zone,ids[[j]])
+  #names(dat_zone)<-c("vg_clss","cor_txt","ent_txt","NDVI","PC1","uvvr_diff","uvvr_mean","precip","tideres","himarsh","id")
+  #mat[[j]]<-as.data.frame(terra::as.matrix(dat_zone[-c(3,6,8)],wide=F))%>%#remove predictors not being used (cor, uvvr_diff, precip)
+  #  mutate(id=paste0(id,"z",j))%>%
+  #  filter(vg_clss!="NaN")
   file_list_all_zones[[j]]<-file_list_zone
 }
 
@@ -389,39 +366,39 @@ file_list_zone<-list()
     file_list_zone<-file_list_zone[file_list_zone!="NULL"]
   }
   #read the list of data files for that zone into a raster stack
-  dat_zone<-rast(unlist(file_list_zone))
+#  dat_zone<-rast(unlist(file_list_zone))
   #add the cell IDs to the stack
-  dat_zone<-rast(list(dat_zone,ids[[1]]))
+#  dat_zone<-rast(list(dat_zone,ids[[1]]))
   #set all cells outside the marsh to NA
-  dat_zone<-mask(dat_zone,ids[[1]])
+#  dat_zone<-mask(dat_zone,ids[[1]])
   #label the datasets
-  names(dat_zone)<-c("vg_clss","cor_txt","ent_txt","NDVI","PC1","uvvr_diff","uvvr_mean","precip","tideres","himarsh","lomarsh","id")
+#  names(dat_zone)<-c("vg_clss","cor_txt","ent_txt","NDVI","PC1","uvvr_diff","uvvr_mean","precip","tideres","himarsh","id")
   #divide zone into parts
-  for(i in 1:n_div){
-   row_start[i]<-((round(nrow(dat_zone)/n_div))*(i-1))+1
-   row_end[i]<-(round(nrow(dat_zone)/n_div))*i
-  }
-  for(i in 1:n_div){
-  dat_zone_sub<-dat_zone[c(row_start[i]:row_end[i]),c(1:ncol(dat_zone)),drop=F] # if it throws an error, might need to sub 1 from the last row end
+#  for(i in 1:n_div){
+#   row_start[i]<-((round(nrow(dat_zone)/n_div))*(i-1))+1
+#   row_end[i]<-(round(nrow(dat_zone)/n_div))*i
+#  }
+#  for(i in 1:n_div){
+#  dat_zone_sub<-dat_zone[c(row_start[i]:row_end[i]),c(1:ncol(dat_zone)),drop=F] # if it throws an error, might need to sub 1 from the last row end
   
   #coerce raster stack into a matrix with datasets (layers) as columns and cells as rows (wide = F will do this)
-  dat_zone_list[[i]]<-as.data.frame(terra::as.matrix(dat_zone_sub[-c(3,6,8,10)],wide=F))%>% #remove variables we arent using
-    mutate(id=paste0(id,"z1"))%>%
+#  dat_zone_list[[i]]<-as.data.frame(terra::as.matrix(dat_zone_sub[-c(3,6,8)],wide=F))%>% #remove variables we arent using
+#    mutate(id=paste0(id,"z1"))%>%
     #remove cells outside the marsh boundary
-    filter(vg_clss!="NaN")
-  }
+#    filter(vg_clss!="NaN")
+#  }
   #add the list of data files for that zone to a list of all zones' datafiles
   file_list_all_zones[[1]]<-file_list_zone
 
   #bind all the zone sections into one dataframe
-  mat[[1]]<-do.call("rbind",dat_zone_list)
+#  mat[[1]]<-do.call("rbind",dat_zone_list)
 
 
   
 #write each zone prediction dataset to file
-for(i in 1:length(mat)){
-  write.csv(mat[[i]],file=paste0(path_out,"/Final_outputs/prediction_surfaces/Z",i,"_prediction_30m.csv"),row.names = F)
-}
+#for(i in 1:length(mat)){
+#  write.csv(mat[[i]],file=paste0(path_out,"/Final_outputs/prediction_surfaces/Z",i,"_prediction_30m.csv"),row.names = F)
+#}
 
 #save the list of predictor files for each zone as an R object
 save(file_list_all_zones,file=paste0(path_out,"/predictor_files_all_zones_",reso,"m.rds"))
