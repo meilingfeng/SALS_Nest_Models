22e24205e5a713ed4bd38ed57afbe0b4a2fdadaf
ggg80 <xgeng3@u.rochester.edu>
2024-03-05 15:26:48 -0500

    +Add summary of species nesting data summary

--- 01d_check_spatiotemporal_data_availability.R
+++ 01d_check_spatiotemporal_data_availability.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(rgdal) #package for geospatial analyses
 library(terra)#updated version of raster package
 library(dismo)
-source("C:/Users/mefen/OneDrive/Documents/Github/UCONN/SHARP/Functions/gridSample_sf.R")
+source("C:/Users/10788/Desktop/SaltMarsh/SHARP/Functions/gridSample_sf.R")
 
 
 ##############################################################################################
@@ -20,7 +20,8 @@ path_out<-"D:/Nest_Models/Outputs/"
 
 ## 1. Format Nest Observations
 # -------------------------------------
-speciesnames<-c("SESP","CLRA","WILL","NESP","HYBR")
+#Record nesting data summary for each species of interest
+speciesnames<-c("SALS","SESP","CLRA","WILL","NESP","HYBR")
 species_sum<-as.data.frame(speciesnames)
 # load nest observation shapefile
 nests_all<-st_read(paste0(path_out,"Final_outputs/Nest_locations/nest_locations_01_3_23.shp"))%>%
@@ -51,6 +52,15 @@ for (j in 1:length(speciesnames)){
   species_sum$site_n[j]<-length(unique(nests_all$site))
   species_sum$year_n[j]<-list(sort(unique(nests$Year)))
   species_sum
+  
+#  speciesnames nest_n fate_n site_n                                                           year_n
+#  1         SALS   2476   2301     42 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020
+#  2         SESP   1078   1036     42             2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019
+#  3         CLRA    394    325     42 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021
+#  4         WILL    357    284     42       2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020
+#  5         NESP    273    271     42       2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020
+#  6         HYBR    100     99     42                                     2011, 2012, 2013, 2014, 2015
+  
   ## 3. write a new file with only nests past 2010 to assume consistent sampling protocol
   #--------------------------------------------------------------------------------
   st_write(nests,paste0(path_out,"Final_outputs/Nest_locations/",speciesnames[j],"_nests_2010_2020.shp"),append = FALSE)
