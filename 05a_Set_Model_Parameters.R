a9a85b249d42d60b10c0a4a06dbaaa4d16152625
Mei-Ling Feng <mefeng7@gmail.com>
2024-05-06 09:46:22 -0400

    Adding time since new moon and date as additional variables for the GLMs. Filtering the data to those with complete date information. Also updated the thinning for nest fails to use terra instead of raster.

--- 05a_Set_Model_Parameters.R
+++ 05a_Set_Model_Parameters.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(sf)
 library(terra)#updated version of raster package
+library(tidyterra)
 library(dismo)
 library(gbm)
 source("C:/Users/mefen/OneDrive/Documents/Github/SHARP/Functions/gridSample_sf.R")
@@ -56,19 +57,167 @@ dat_comp<-dat[complete.cases(dat[,all_terms]),]
 dat[!(dat$id%in%dat_comp$id),]
 
 
+# Add some additional nuisance variables
+#--------------------------------------------------------------------------
+# 1. Time nest was initiated since the last new moon - estimate of how late in the flood free window the nest was started
+# later nests, more time since last moon, have less time to fledge before the nest highest high tide. 
+moon<-read.csv(paste0(dat_path,"Environmental Predictors/new_moon_dates.csv"))%>%
+  mutate(new.moon.date=mdy(new.moon.date))%>%
+  arrange(new.moon.date)%>%
+  mutate(lag=lead(new.moon.date))%>%
+  filter(!is.na(lag))
+
+#create a list of potential dates that come after each moon date to join to the nest data
+moon.dates<-list()
+for(i in 1:nrow(moon)){
+  moon.dates[[i]]<-seq(moon[i,]$new.moon.date,moon[i,]$lag,by="days")
+}
+
+#add to each new.moon.date
+moon.dat<-expand.grid(new.moon.date=moon$new.moon.date, date=do.call("c",moon.dates))
+
+moon.dat<-moon.dat%>%
+  # subtract the new moon date from the survey date to get days since the new moon
+  mutate(time_since_moon=date-new.moon.date)%>%
+  # moon cycle is 29 days, so time should be more than 0 but less than 29 days
+  filter(time_since_moon>=0&time_since_moon<=29)%>%
+  distinct(.keep_all = T)%>%
+  mutate(time_since_moon=as.numeric(time_since_moon))%>%
+  dplyr::select(-new.moon.date)
+
+#get all possible first egg dates using nest monitoring data
+mon<-read.csv(paste0(dat_path,"Demographic Database/Nest_Monitoring_May_24.csv"))
+#replace all missing chick age values with NA
+mon[,10:25]<-replace(mon[,10:25],mon[,10:25]==""|mon[,10:25]=="NOT REC"|mon[,10:25]=="na",NA)
+
+#Calculate hatch dates based on chick age for records missing first egg dates.
+mon2<-mon%>%
+  #filter the NA ages out
+  filter(if_any(contains("Chick.Age"),~!is.na(.x)))%>%
+  #keep only one observation per nest
+  distinct(SHARP.Nest,.keep_all = T)%>%
+  rowwise()%>%
+  #calculate the oldest chick age for the nest
+  mutate(max_age=as.numeric(max(c_across(contains("Chick.Age")),na.rm = T)),
+         # estimate the hatch date by subtracting the oldest chick age from the survey date
+         hatch.date=mdy(Visit.Date)-days(max_age))%>%
+  dplyr::select(id=SHARP.Nest,hatch.date)%>%
+  ungroup()
+
+#also get all records with hatch dates from nest fates data
+add.hatch<-read.csv(paste0(dat_path,"Demographic Database/NestFates_2001-2020.csv"))%>%
+  dplyr::select(id=SHARPNestID,hatch.date=EstHatchDate)%>%
+  filter(!(hatch.date%in%c("Unknown","UNK","NOT REC",""))&!is.na(hatch.date))%>%
+  mutate(hatch.date=mdy(hatch.date))
+
+#remove any age estimated hatch dates that already have estimate dates in the fates data
+mon3<-mon2%>%
+  filter(!(id%in%c(add.hatch$id)))
+
+#join all the estimates hatch dates
+all.add.hatch<-rbind(mon3,add.hatch)%>%
+         # subtract 10 incubation days from the hatch date, this is the day the last egg was laid per Ruskin 2016 (Field 2018 Ecography paper)
+         mutate(inc.date=hatch.date-days(10))
+
+# calculate total eggs
+mon<-mon%>%
+  rowwise()%>%
+  mutate(Eggs=as.numeric(Eggs),
+         Nestlings=as.numeric(Nestlings))%>%
+  mutate(total.eggs=sum(c(Eggs,Nestlings),na.rm=T))%>%
+  ungroup()
+
+max.eggs<-read.csv(paste0(dat_path,"Demographic Database/NestFates_2001-2020.csv"))%>%
+  dplyr::select(id=SHARPNestID,total.eggs=MaxNumEggs)%>%
+  filter(!(total.eggs%in%c("UNKNOWN","UNK","NOT REC",""))&!is.na(total.eggs))%>%
+  mutate(total.eggs=as.numeric(total.eggs))
+
+#remove any total egg counts that already have estimates in the fates data
+mon4<-mon%>%
+  filter(!(SHARP.Nest%in%c(max.eggs$id)))%>%
+  dplyr::select(id=SHARP.Nest,total.eggs)%>%
+  distinct(id,.keep_all = T)
+#combine
+all.add.firstegg<-rbind(mon4,max.eggs)%>%
+  right_join(all.add.hatch,by="id")%>%
+  #estimated first egg date is last egg date-(N eggs-1)
+  mutate(date2=inc.date-days(total.eggs-1))%>%
+  dplyr::select(id,date2)
+
+
+dat_comp<- read.csv(paste0(dat_path,"Demographic Database/NestFates_2001-2020.csv"))%>%
+  dplyr::select(id=SHARPNestID, date=EstFirstEggDate)%>%
+  mutate(date=mdy(date))%>%
+  #first add estimated first egg dates from fates data to the cleaned nesting data
+  right_join(dat_comp,by="id")
+
+#how many observations does calculating first egg date fix? (323)
+missing<-dat_comp[is.na(dat_comp$date),]
+nrow(missing[missing$id%in%all.add.firstegg$id,])
+
+dat_comp2<- dat_comp%>%
+  #then add the calculated first egg dates for those that are missing
+  left_join(all.add.firstegg,by="id")%>%
+  mutate(date=coalesce(date,date2))%>%#coalesce takes values from date unless they are NA, then takes from date2
+  dplyr::select(-date2)
+
+dat_comp2<-dat_comp2%>%
+  left_join(moon.dat,by="date")%>%
+  distinct(id,.keep_all = T)
+
+# 2. Day of the year. Expect nest site selection to change as vegetation grows in.
+dat_comp2<-dat_comp2%>%
+  mutate(doy=yday(date))
+
+
+
+
+
+# Check for complete records again
+#-------------------------------------------------------------------------------
+#filter only complete records, those with first egg dates and background points
+dat_comp3<-dat_comp2[!is.na(dat_comp2$doy)|dat_comp2$presence==0,]
+dat_comp3<-dat_comp3%>%dplyr::filter(!is.na(region))
+
 
 ### Check Class Balance
 #---------------------------------------------------------------------------------------------
 # balancing can help machine learning models 
 
 
-## 1. divide observations into nest presence and nest survival datasets
-pres_dat<-dat_comp
-surv_dat<-dat_comp[!is.na(dat_comp$fate),] #filter just the nests with known fates
+# randomly select the same number of background points as the filtered, complete nest records
+# sample the same sample size as each region
+reg_size<-as.vector(table(dat_comp3[dat_comp3$presence==1,]$region))
+regions<-sort(unique(dat_comp3[dat_comp3$presence==1,]$region))
+temp1<-list()
+
+for(i in 1:length(reg_size)){
+  temp1[[i]]<-dat_comp3[dat_comp3$region==regions[[i]]&dat_comp3$presence==0,]
+  temp1[[i]]<-temp1[[i]][sample(1:nrow(temp1[[i]]), reg_size[i]), ]
+}
+temp1[[length(reg_size)+1]]<-dat_comp3[dat_comp3$presence==1,]
+dat_comp4 <- do.call("rbind", temp1)
+dat_comp4<-dplyr::filter(dat_comp4,!is.na(id))
+
+#fill in day of year for background points by assigning each bp to a nest observation date
+date_order<-list()
+#drawn random index numbers for all the dates in each region
+for(i in 1:length(reg_size)){
+date_order[[i]]<-sample(1:reg_size[i],reg_size[i],replace = F)
+dat_comp4[dat_comp4$presence==0 & dat_comp4$region==regions[[i]], ]$date<-dat_comp4[dat_comp4$presence==1 & dat_comp4$region==regions[[i]], ]$date[date_order[[i]]]
+}
+
+dat_comp4<-dat_comp4%>%
+  mutate(doy=yday(date))
 
 
+##  divide observations into nest presence and nest survival datasets
+pres_dat<-dat_comp4
+surv_dat<-dat_comp4[!is.na(dat_comp4$fate),] #filter just the nests with known fates
+surv_dat<-surv_dat[!(is.na(surv_dat$time_since_moon)),]#removes one 2021 observation
 
-## 2. look at the prevalence of:
+
+## look at the prevalence of:
 
 # nest fledges to nest fails
 addmargins(table(surv_dat$fate));sum(surv_dat$fate)/nrow(surv_dat)
@@ -79,112 +228,41 @@ addmargins(table(pres_dat$presence)); sum(pres_dat$presence)/nrow(pres_dat)
 
 
 
-# plot proportion of fledges (nest success) across sites
-success_site<-dat%>%group_by(site)%>%
-  filter(!is.na(fate))%>%
-  summarize(pr_success=sum(fate,na.rm=T)/n(),
-            lat=mean(latitude,na.rm=T))
-
-hist(success_site$pr_success, breaks=seq(0,1,0.1))
-plot(success_site$pr_success~success_site$lat,xlab="Site latitude",ylab="Proportion of nests fledging")
-#nest success appears to be more variable at higher latitudes
-mean(success_site$pr_success)
-#nest success across sites is also 32% on average
-
-
-
-## 3. Check sampling bias
-
-# look at spatial distribution and temporal distribution
-# does the data need thinning?
-# best to thin the more prevalent class (nest fails) to improve calibration - thin to balance
-
-# How are observations distributed across marsh sites?
-
-n_nest_per_site<-summarise(group_by(dat,site),n_nests=n())%>%filter(!(is.na(site)))
-hist(n_nest_per_site$n_nests) 
-length(n_nest_per_site[n_nest_per_site$n_nests<251,]$n_nests)/nrow(n_nest_per_site)
-#96% of sites have 250 or less nest observations, but a couple have more than 300 observations.
-
-#create lat bins
-n_nest_per_site<-dat%>%
-  mutate(lat_bin = cut(latitude, breaks=8))%>%
-  dplyr::select(site,lat_bin)%>%
-  distinct()%>%
-  right_join(n_nest_per_site,by="site")
-ggplot(n_nest_per_site, aes(x=lat_bin, y=n_nests))+
-  geom_boxplot()+
-  labs(y= "Number of nest observations", x="Geographic Region")+
-  geom_jitter(width = 0.2)
-# some difference in sampling intensity throughout region, but nothing seems overly under sampled
-# lat 42 has the least sites
-
-
-
-#16 sites have at least 5 years of sampling
-#Site AT, ER, HM, ID, MN, OC all have 6 years of sampling
-#Sites EL and CL have the most years of sampling (8 and 7 years)
-n_yr_per_site<-summarise(group_by(dat,site),n_years=length(unique(Year)))
-n_yr_per_site
-ggplot(n_yr_per_site)+
-  geom_bar(aes(x=n_years))+
-  labs(y="Number of Sites",x="Number of Years Sampled")+
-  theme_bw()
-
-
-#Which years sampled the most, for which sites?
-#seems like most sites with longer monitoring efforts began in 2011 and continue through 2020
-#Most sites sampled only once occured before 2010, but a few one-off sites were also sampled 2011-2020
-ggplot(left_join(distinct(dat[,c("Year","site")], .keep_all = T),n_yr_per_site,by="site"))+
-  geom_bar(aes(x=Year,fill=as.factor(n_years)))+
-  scale_fill_viridis_d()+
-  labs(fill="N Years\nSite Sampled", x="Year",y="Number of Sites Sampled")+
-  theme_bw()
-
-n_nest_per_siteyr<-summarise(group_by(filter(dat,bp!="b"),site,Year),n_nests=n())
-arrange(n_nest_per_siteyr,desc(n_nests))
-
-# 2011-2015 has the most sites continuously sampled
-
-
-
-
-# Thin to balance nest failures 
+# Thin to balance nest failures if there are a lot of them (zeros)
+if(sum(surv_dat$fate)/nrow(surv_dat)>0.59|sum(surv_dat$fate)/nrow(surv_dat)<0.41){
 # create a RasterLayer with the extent of nest locations
-points<-st_read(paste0(path_out,"Final_outputs/Nest_locations/SALS_nests_2010_2020_dist_err_removed.shp"))%>%
+points<-vect(paste0(path_out,"Final_outputs/Nest_locations/SALS_nests_2010_2020_dist_err_removed.shp"))%>%
   # project to UTM so we can specify resolution in meters
-  st_transform("EPSG:26918")
+  project("EPSG:26918")
 # set the resolution of the cells to 30 m
 res<- 30
 r <- rast(extent=ext(points), resolution = res, crs="EPSG:26918")
 
 # thin nest fails down to 1 nest per 30 meters
 fails<-points%>%
-  filter(fate==0)
+  tidyterra::filter(fate==0)
 
 non_fails<-points%>%
-  filter(fate!=0|is.na(fate))
+  tidyterra::filter(fate!=0|is.na(fate))
 
-nest_thin <- gridSample(fails, r, n=1)
+nest_thin <- spatSample(fails, size=1, strata=r)#strata are the 30m raster cells. Sample 1 point per cell.
 
-fails$Easting<-sf::st_coordinates(fails)[,1]
-fails$Northing = sf::st_coordinates(fails)[,2]
-fails_ss<- filter(fails,(Easting%in%nest_thin[,1]&Northing%in%nest_thin[,2]))%>%
-  distinct(geometry,.keep_all = T)
+fails_ss<- filter(fails,id%in%nest_thin$id)# select only the thinned points
 
 #number of nest records removed N=562
 nrow(fails)-nrow(fails_ss)
 
 #recombine subset fails and fledges
-nests_ss<-rbind(fails_ss%>%dplyr::select(-Easting,-Northing),non_fails)
+nests_ss<-rbind(fails_ss,non_fails)
 
 
 #balance survival data by filtering for the thinned record ids
 surv_dat<-surv_dat%>%
+  # all the successes and any fails that made it through the thinning
   filter(fate==1|id%in%fails_ss$id)
 
 table(surv_dat$fate);sum(surv_dat$fate)/nrow(surv_dat) # get close to 0.5 prevelance 
-
+}
 
 
 # thin the veg data if using as background points
@@ -231,7 +309,7 @@ if(ab_type=="v"){
 nrow(pres_dat) 
 #There are 4728 nest observations
 nrow(surv_dat)
-#There are 1611 nest fate observations
+#There are 1611 nest fate observations (now 1528)
 
 length(unique(pres_dat$site))
 #32 sites
@@ -252,7 +330,7 @@ surv_dat$group<-kfold(surv_dat,k)
 
 
 #check balance in each split dataset
-table(pres_dat$presence,pres_dat$group) #retains same balance
+table(pres_dat$presence,pres_dat$group) # stays mostly well balanced
 table(surv_dat$fate,surv_dat$group)
 
 
@@ -262,17 +340,6 @@ pres_dat$y<-pres_dat$presence
 
 surv_dat$y<-surv_dat$fate
 
-#*try using background points for success models***
-#t<-rbind(pres_dat[pres_dat$bp=="b",],surv_dat[surv_dat$fate==1,])
-#t_f<-t[t$y==0,]
-#t_s<-t[t$y==1,]
-#t_sum<-summarize(group_by(t_s,region),n=n())
-#t_f<-rbind(t_f[sample(which(t_f$region==1),as.numeric(t_sum[1,2])),],
-#                    t_f[sample(which(t_f$region==3),as.numeric(t_sum[2,2])),],
-#                    t_f[sample(which(t_f$region==4),as.numeric(t_sum[3,2])),],
-#                    t_f[sample(which(t_f$region==5),as.numeric(t_sum[4,2])),])
-#surv_dat<-rbind(t_f,t_s)
-#***
 
 table(pres_dat$y)
 table(surv_dat$y)
